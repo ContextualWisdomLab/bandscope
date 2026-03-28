fd8cfa65605d66b9e44b7f7d9110dc7b0c5ee207
Seongho Bae <me@seonghobae.me>
2026-03-28 21:05:01 +0900

    style: sort imports in chords module

--- services/analysis-engine/src/bandscope_analysis/chords/__init__.py
+++ services/analysis-engine/src/bandscope_analysis/chords/__init__.py
@@ -1,8 +1,8 @@
 """Chord analysis module for extracting harmonic content from sections."""
 
 from .analyzer import ChordAnalyzer
-from .model import ChordAnalysisResult, ChordLabel, SectionChordSummary
 from .capo import detect_capo_and_tuning
+from .model import ChordAnalysisResult, ChordLabel, SectionChordSummary
 
 __all__ = [
     "ChordAnalyzer",
