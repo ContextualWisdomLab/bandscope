ef5242bf15dd4af7fe82ba0cbff24180e82836db
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-06-29 13:22:15 +0000

    🧪 cn() 유틸리티 함수에 대한 단위 테스트 추가

--- apps/desktop/src/lib/utils.test.ts
+++ apps/desktop/src/lib/utils.test.ts
@@ -7,8 +7,9 @@ describe("cn", () => {
   })
 
   it("handles conditional classes", () => {
-    // eslint-disable-next-line no-constant-binary-expression
-    expect(cn("foo", true && "bar", false && "baz")).toBe("foo bar")
+    const applyBar = true;
+    const applyBaz = false;
+    expect(cn("foo", applyBar && "bar", applyBaz && "baz")).toBe("foo bar")
   })
 
   it("merges arrays of class names", () => {
