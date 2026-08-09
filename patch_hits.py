783ce0cecf92c0c9683629525c5a8d9e40964435
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-08-09 10:35:58 +0000

    test: test_hits.py에 에러 경로 테스트 추가

--- patch_hits.py
+++
@@ -1,7 +0,0 @@
-import os
-
-file_path = "services/analysis-engine/src/bandscope_analysis/temporal/hits.py"
-with open(file_path, "r") as f:
-    content = f.read()
-
-# Make sure we don't accidentally revert anything, the current error seems to stem from numpy internal errors on specific inputs with onset_detect
